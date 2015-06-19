3666e7e97ece4df7a7a5e95605d8b6a48c320b82
Jun Zhong <jzhong@accela.com>
2015-06-19 12:12:49 -0700

    Add unit test for AMRequest

--- sdk/app/src/test/java/com/accela/mobile/AMRequestTest.java
+++ sdk/app/src/test/java/com/accela/mobile/AMRequestTest.java
@@ -1,23 +1,28 @@
 package com.accela.mobile;
 
+import com.accela.mobile.data.TestDataHelper;
 import com.accela.mobile.http.AsyncHttpClient;
 import com.accela.mobile.http.RequestParams;
+import com.accela.mobile.http.SyncHttpClient;
 
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.ProtocolVersion;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHttpResponse;
 import org.apache.http.message.BasicStatusLine;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.robolectric.Robolectric;
-import org.robolectric.tester.org.apache.http.HttpEntityStub;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 
@@ -25,45 +30,33 @@ import java.io.UnsupportedEncodingException;
  * Created by jzhong on 6/17/15.
  */
 
-@PrepareForTest( {AMRequest.class, AsyncHttpClient.class})
-public class AMRequestTest extends TestCaseBase{
+@PrepareForTest( {AMRequest.class, DefaultHttpClient.class, SyncHttpClient.class, AsyncHttpClient.class})
+public class AMRequestTest extends PowerMockTestCaseBase{
 
     AccelaMobile accelaMobile;
     String appId = "appId12345";
     String appSecret = "appSecret12345";
 
-
-    class CountingResponseRule implements HttpEntityStub.ResponseRule {
-
-        private int mCounter = 0;
-
-        @Override
-        public HttpResponse getResponse() throws HttpException, IOException {
-            HttpResponse response= new BasicHttpResponse(
-                    new ProtocolVersion("HTTP", 1, 1), 200, "OK");
-            response.setEntity(new StringEntity(Integer.toString(mCounter)));
-            mCounter++;
-
-            return response;
-        }
-
-        @Override
-        public boolean matches(HttpRequest request) {
-            String uri = request.getRequestLine().getUri();
-            return true;
-           /* if (uri.equals("https://apis.accela.com")) {
-                return true;
-            }
-            return false; */
-        }
-    }
-
+    JSONObject expectedJsonReturn;
 
     @Before
     public void before() throws Exception {
         accelaMobile = new AccelaMobile(Robolectric.application, appId, appSecret);
-        Robolectric.getFakeHttpLayer().addHttpResponseRule(
-                new CountingResponseRule());
+        //Robolectric.getFakeHttpLayer().addHttpResponseRule(new CountingResponseRule());
+
+        //create expected json return;
+        expectedJsonReturn = new JSONObject();
+
+        String status = "0";
+        JSONArray array = new JSONArray();
+        for(int i= 0; i< 5; i++) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put(String.format("%d", i), "json-object3");
+            array.put(i, jsonObject);
+        }
+        expectedJsonReturn.put("status", status);
+        expectedJsonReturn.put("result", array);
+
     }
 
     private HttpResponse prepareResponse(int expectedResponseStatus,
@@ -85,53 +78,39 @@ public class AMRequestTest extends TestCaseBase{
         RequestParams postData = new RequestParams();
         AMRequest.HTTPMethod httpMethod = AMRequest.HTTPMethod.GET;
         String serviceURL = "https://apis.accela.com";
-       // DefaultHttpClient mockHttpClient = new MockHttpClient();
-       // PowerMockito.whenNew(DefaultHttpClient.class).withAnyArguments().thenReturn(mockHttpClient);
-
-        //MockSyncHttpClient syncHttpClient = new MockSyncHttpClient();
-        //PowerMockito.whenNew(AsyncHttpClient.class).withAnyArguments().thenReturn(new AsyncHttpClient());
 
 
-        /*
-        PowerMockito.whenNew(DefaultHttpClient.class).withAnyArguments().then(new Answer<Object>() {
-            @Override
-            public DefaultHttpClient answer(InvocationOnMock invocation) throws Throwable {
-                DefaultHttpClient httpClient = new MockHttpClient();
-                HttpResponse response = prepareResponse(200, "{'status': 0}");
-                HttpClient mockHttpClient = Mockito.mock(HttpClient.class);
-                Mockito.when(mockHttpClient.execute(Mockito.any(HttpUriRequest.class)))
-                        .thenReturn(response);
-                return httpClient;
-            }
-        });*/
+        //power mock DefaultHttpClient
+        PowerMockito.mockStatic(DefaultHttpClient.class);
 
-        HttpResponse response = prepareResponse(200, "{'status': 0}");
+        //create mockHttpClient
+        DefaultHttpClient mockHttpClient = PowerMockito.mock(DefaultHttpClient.class);;
+        PowerMockito.whenNew(DefaultHttpClient.class).withAnyArguments().thenReturn(mockHttpClient);
 
-       // Mockito.when(mockHttpClient.execute(Mockito.any(HttpUriRequest.class))).thenReturn(response);
-
-        AMRequest request = new AMRequest(accelaMobile, serviceURL, params, httpMethod);
-
-        JSONObject reponse  = request.fetch(postData);
-
-        /*
-       request.sendRequest(postData, new AMRequestDelegate() {
-            @Override
-            public void onSuccess(JSONObject response) {
-                assertTrue(true);
-            }
-
-            @Override
-            public void onFailure(AMError error) {
-                assertTrue(true);
-            }
-
-            @Override
-            public void onStart() {
-
+        //mock httpclient.execute
+        PowerMockito.when(mockHttpClient.execute(Matchers.any(HttpUriRequest.class)) ).thenAnswer(new Answer() {
+            public Object answer(InvocationOnMock invocation) {
+                Object[] args = invocation.getArguments();
+                Object mock = invocation.getMock();
+                if(args.length == 1 && (args[0] instanceof HttpUriRequest)) {
+                    HttpResponse response = TestDataHelper.makeResponse(200, expectedJsonReturn.toString());
+                    return response;
+                }
+                return null;
             }
         });
-        */
+
+
+
+        AMRequest request = new AMRequest(accelaMobile, serviceURL, params, httpMethod);
+        JSONObject reponse  = request.fetch(postData);
+
+        TestUtils.assertEquals(expectedJsonReturn, reponse);
     }
+
+
+
+
 
 
 
