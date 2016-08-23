755c55414ad21a3aa74ce7fb4b20cf8ea641a363
EricYang505 <ericyoung505@gmail.com>
2016-08-22 17:17:12 -0700

    add copyright

--- sdk/library_sdk/src/main/java/com/accela/mobile/HttpResponseLogHandler.java
+++ sdk/library_sdk/src/main/java/com/accela/mobile/HttpResponseLogHandler.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2015 Accela, Inc.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * Accela.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 package com.accela.mobile;
 
 /**
@@ -5,17 +23,12 @@ package com.accela.mobile;
  */
 
 
-import org.json.JSONObject;
-
 import android.content.Intent;
 
-import com.accela.mobile.AMError;
-import com.accela.mobile.AMLogger;
-import com.accela.mobile.AMRequest;
-import com.accela.mobile.AMSetting;
-import com.accela.mobile.AuthorizationManager;
 import com.accela.mobile.http.RequestParams;
 import com.accela.mobile.http.volley.AuthFailureError;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
--- sdk/library_sdk/src/main/java/com/accela/mobile/http/AMMultiPartRequest.java
+++ sdk/library_sdk/src/main/java/com/accela/mobile/http/AMMultiPartRequest.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2015 Accela, Inc.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * Accela.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
 package com.accela.mobile.http;
 
 import android.os.AsyncTask;
@@ -5,7 +22,6 @@ import android.os.SystemClock;
 
 import com.accela.mobile.AMDocRequestManager;
 import com.accela.mobile.AMError;
-import com.accela.mobile.AMLogger;
 import com.accela.mobile.AMRequest;
 import com.accela.mobile.AMRequestDelegate;
 import com.accela.mobile.http.volley.Legacy.BasicHeader;
@@ -28,7 +44,6 @@ import com.accela.mobile.http.volley.toolbox.PoolingByteArrayOutputStream;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
